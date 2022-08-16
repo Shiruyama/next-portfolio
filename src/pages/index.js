56e63d812357f8e92e36b2680571c4d093415d02
rmgh <rmgh.shmoritz@gmail.com>
2022-08-16 20:42:47 +0900

    baseUrl変更とディレクトリ構造変更の設定

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<>
+			<div className="container">
+				<h1 className="text-3xl font-bold underline text-red-500 text-center">
+					Hello world!
+				</h1>
+			</div>
+		</>
+	);
+}
